e017b7688838fe06de719f3f0e30dcd77707648f
Karim Altir <mhd.altir@student.ehb.be>
2023-12-03 14:25:47 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KlantenService_Steam_Framework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
