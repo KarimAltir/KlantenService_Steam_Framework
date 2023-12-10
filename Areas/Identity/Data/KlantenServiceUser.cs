e605e3e2f8d4d0fd376562439c07e8a00f4d4914
Karim Altir <mhd.altir@student.ehb.be>
2023-12-10 17:37:02 +0100

    Create KlantenServiceUser

---
+++ Areas/Identity/Data/KlantenServiceUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace KlantenService_Steam_Framework.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the KlantenServiceUser class
+public class KlantenServiceUser : IdentityUser
+{
+    //adding first name
+    public string FirstName { get; set; }
+
+    //adding last name
+    public string LastName { get; set; }
+}
+
