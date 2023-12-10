ac1e8ea70a0297dbdc8ad4c99ac1b0e6252df7a3
Karim Altir <mhd.altir@student.ehb.be>
2023-12-10 17:38:15 +0100

    change the background image into string

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Policy;
+﻿using System.Drawing;
+using System.Security.Policy;
 
 namespace KlantenService_Steam_Framework.Models
 {
@@ -6,7 +7,7 @@ namespace KlantenService_Steam_Framework.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Url Background_image { get; set; }
+        public string Background_image { get; set; }
         public int Games_count { get; set; }
 
         // De verzameling Complaints die aan dit Game zijn gekoppeld
