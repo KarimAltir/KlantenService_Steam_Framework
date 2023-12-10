bd58463ce2cfd9bb97375320ccdb246f283531b9
Karim Altir <mhd.altir@student.ehb.be>
2023-12-10 17:30:59 +0100

    added Identity names for the migrations

--- Areas/Identity/Data/Migrations/20231210141941_IdentityNames.Designer.cs
+++ Areas/Identity/Data/Migrations/20231210141941_IdentityNames.Designer.cs
@@ -4,6 +4,7 @@ using KlantenService_Steam_Framework.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KlantenService_Steam_Framework.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231210141941_IdentityNames")]
+    partial class IdentityNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
