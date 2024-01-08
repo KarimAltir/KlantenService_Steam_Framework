d38a539f03e25e6f717ffa894a5529ca80227ffd
Karim Altir <mhd.altir@student.ehb.be>
2024-01-08 20:40:23 +0100

    added MailService + API services

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-using KlantenService_Steam_Framework.Areas.Identity.Data;
+﻿using KlantenService_Steam_Framework.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using KlantenService_Steam_Framework.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc.Razor;
+using NETCore.MailKit.Infrastructure.Internal;
+using static KlantenService_Steam_Framework.Services.MailService;
+using KlantenService_Steam_Framework;
+using Microsoft.OpenApi.Models;
 
 internal class Program
 {
@@ -18,7 +24,38 @@ internal class Program
         builder.Services.AddDefaultIdentity<KlantenServiceUser>((options) => options.SignIn.RequireConfirmedAccount = true)
             .AddRoles<IdentityRole>()
             .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        builder.Services.AddTransient<IEmailSender, MailKitEmailSender>();
+
+        // De volgende configuratie van de MailKit wordt toegevoegd als demonstratie, maar gebruiken we niet.
+        // Deze is "overschreven" door het gebruik van de database-parameters in Globals, en ge�nitialiseerd in de data Initializer
+        builder.Services.Configure<MailKitOptions>(options =>
+        {
+            options.Server = builder.Configuration["ExternalProviders:MailKit:SMTP:Address"];
+            options.Port = Convert.ToInt32(builder.Configuration["ExternalProviders:MailKit:SMTP:Port"]);
+            options.Account = builder.Configuration["ExternalProviders:MailKit:SMTP:Account"];
+            options.Password = builder.Configuration["ExternalProviders:MailKit:SMTP:Password"];
+            options.SenderEmail = builder.Configuration["ExternalProviders:MailKit:SMTP:SenderEmail"];
+            options.SenderName = builder.Configuration["ExternalProviders:MailKit:SMTP:SenderName"];
+            options.Security = true;  // true zet ssl or tls aan
+        });
+
+        // Add services for globalization/localization
+        builder.Services.AddLocalization(options => options.ResourcesPath = "Trssanslations");
+        builder.Services.AddMvc()
+            .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+            .AddDataAnnotationsLocalization();
+
+
         builder.Services.AddControllersWithViews();
+
+        //Add services for RESTFull API
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen(c =>
+                            {   
+                                c.SwaggerDoc("v1", 
+                                new OpenApiInfo { Title = "ApplicationName", Version = "v1" });
+                            });
 
         builder.Services.Configure<IdentityOptions>(options =>
         {
@@ -43,17 +80,24 @@ internal class Program
 
         var app = builder.Build();
 
+        Globals.App = app;          // Zorgt dat dit altijd een instantie van de huidige app bijhoudt
+
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
         }
+        else
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApplicationName v1"); });
+        }
 
-        app.UseHttpsRedirection();
         app.UseStaticFiles();
 
         app.UseRouting();
-
+        app.UseAuthentication();
         app.UseAuthorization();
 
         using (var scope = app.Services.CreateScope())
@@ -68,6 +112,8 @@ internal class Program
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
         app.MapRazorPages();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
         app.Run();
     }
