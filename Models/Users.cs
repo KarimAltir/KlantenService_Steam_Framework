ba04055b0bb0d5068ce1c5c86b0fd4318220c1c8
Karim Altir <mhd.altir@student.ehb.be>
2024-01-08 20:41:44 +0100

    created Users class

---
+++ Models/Users.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KlantenService_Steam_Framework.Models
+{
+    public class UserIndexViewModel
+    {
+
+        [Display(Name = "Gebruiker")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Voornaam")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Familienaam")]
+        public string LastName { get; set; }
+
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Display(Name = "Geblokkeerd")]
+        public bool Blocked { get; set; }
+
+        [Display(Name = "Rollen")]
+        public List<string> Roles { get; set; }
+
+    }
+
+    public class UserRolesViewModel
+    {
+        [Display(Name = "Gebruiker")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Rollen")]
+        public List<string> Roles { get; set; }
+    }
+}
