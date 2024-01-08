e3d7724a297f693661e2c949bc00eff3594da352
Karim Altir <mhd.altir@student.ehb.be>
2024-01-08 20:41:25 +0100

    added Display name

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,17 +1,28 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 using System.Security.Policy;
 
 namespace KlantenService_Steam_Framework.Models
 {
     public class Game
     {
+
+        //TODO: contolleren voor de namen van de attributen
         public int Id { get; set; }
+
+        [Display(Name = "Naam")]
         public string Name { get; set; }
+
+        [Display(Name = "Afbeelding")]
         public string Background_image { get; set; }
+
+        [Display(Name = "Spel optellen")]
         public int Games_count { get; set; }
 
         // De verzameling Complaints die aan dit Game zijn gekoppeld
         //public ICollection<Complaint> Complaints { get; set; }
 
     }
+
+
 }
--- Models/ProblemType.cs
+++ Models/ProblemType.cs
@@ -1,8 +1,12 @@
-﻿namespace KlantenService_Steam_Framework.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KlantenService_Steam_Framework.Models
 {
     public class ProblemType
     {
         public int Id { get; set; }
+
+        [Display(Name = "Type naam")]
         public string TypeName { get; set; }
 
         // De verzameling Complaints die aan dit probleemtype zijn gekoppeld
