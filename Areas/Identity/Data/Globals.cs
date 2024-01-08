69d789d0286cdac19c02c089fd53998740fd6b65
Karim Altir <mhd.altir@student.ehb.be>
2024-01-08 20:45:54 +0100

    created mail service

---
+++ Areas/Identity/Data/Globals.cs
@@ -0,0 +1,33 @@
+﻿using KlantenService_Steam_Framework.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using KlantenService_Steam_Framework.Areas.Identity.Data;
+using Microsoft.Extensions.Options;
+using NETCore.MailKit.Infrastructure.Internal;
+using static KlantenService_Steam_Framework.Services.MailService;
+
+namespace KlantenService_Steam_Framework
+{
+    static public class Globals
+    {
+        static public KlantenServiceUser DummyUser { get; set; }
+
+        static public Dictionary<string, Parameter> Parameters { get; set; }
+
+        static public WebApplication App { get; set; }
+
+        static public void ConfigureMail()
+        {
+            MailKitEmailSender mailSender = (MailKitEmailSender)App.Services.GetService<IEmailSender>();
+            var options = mailSender.Options;
+            options.Server = Parameters["Mail.Server"].Value;
+            options.Server = Parameters["Mail.Server"].Value;
+            options.Port = Convert.ToInt32(Parameters["Mail.Port"].Value);
+            options.Account = Parameters["Mail.Account"].Value;
+            options.Password = Parameters["Mail.Password"].Value;
+            options.SenderEmail = Parameters["Mail.SenderEmail"].Value;
+            options.SenderName = Parameters["Mail.SenderName"].Value;
+            options.Security = Convert.ToBoolean(Parameters["Mail.Security"].Value);
+
+        }   
+    }
+}
---
+++ Services/MailService.cs
@@ -0,0 +1,59 @@
+﻿using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit.Text;
+using MimeKit;
+using NETCore.MailKit.Infrastructure.Internal;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace KlantenService_Steam_Framework.Services
+{
+    public class MailService
+    {
+        public class MailKitEmailSender : IEmailSender
+        {
+            public MailKitEmailSender(IOptions<MailKitOptions> options)
+            {
+                this.Options = options.Value;
+            }
+
+            public MailKitOptions Options { get; set; }
+
+            public Task SendEmailAsync(string email, string subject, string message)
+            {
+                return Execute(email, subject, message);
+            }
+
+            public Task Execute(string to, string subject, string message)
+            {
+                // create message
+                var email = new MimeMessage();
+                email.Sender = MailboxAddress.Parse(Options.SenderEmail);
+                if (!string.IsNullOrEmpty(Options.SenderName))
+                    email.Sender.Name = Options.SenderName;
+                email.From.Add(email.Sender);
+                email.To.Add(MailboxAddress.Parse(to));
+                email.Subject = subject;
+                email.Body = new TextPart(TextFormat.Html) { Text = message };
+
+                // send email
+                using (var smtp = new SmtpClient())
+                {
+                    SecureSocketOptions Security = SecureSocketOptions.None;
+                    if (Options.Security)
+                    {
+                        if (Options.Port == 587)
+                            Security = SecureSocketOptions.StartTls;
+                        else
+                            Security = SecureSocketOptions.SslOnConnect;
+                    }
+                    //smtp.Connect(Options.Server, Options.Port, Security);
+                    smtp.Authenticate(Options.Account, Options.Password);
+                    smtp.Send(email);
+                    smtp.Disconnect(true);
+                }
+                return Task.FromResult(true);
+            }
+        }
+    }
+}
