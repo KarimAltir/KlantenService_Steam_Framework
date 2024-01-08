c1d8e18b0a72bd30ee9de3c6fb82a8504d4099f6
Karim Altir <mhd.altir@student.ehb.be>
2024-01-08 20:35:41 +0100

    created migration for the users

---
+++ Areas/Identity/Data/Migrations/20240103135846_UsersIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KlantenService_Steam_Framework.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
