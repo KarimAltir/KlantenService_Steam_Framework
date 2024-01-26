5d39221c38f4b328a8e7fef056ce51a2795ef978
Karim Altir <mhd.altir@student.ehb.be>
2024-01-26 22:09:07 +0100

    delete the create, edit and delete methodes from the GamesController

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -16,6 +16,7 @@ namespace KlantenService_Steam_Framework.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly GameService _gameService;
+        //private static Boolean hasbeenUpdated = false;
 
 
         public GamesController(ApplicationDbContext context)
@@ -29,6 +30,26 @@ namespace KlantenService_Steam_Framework.Controllers
             _gameService = new GameService(httpClient);
         }
 
+        //// GET: Games
+        //public async Task<IActionResult> Index()
+        //{
+        //    //return View(await _context.Game.ToListAsync());
+
+
+        //    // Roep de nieuwe UpdateGamesAsync-methode aan
+        //    if (!hasbeenUpdated)
+        //    {
+
+        //        // Haal de games op vanuit de API
+        //        var games = await _gameService.GetGamesAsync();
+        //        List<Game> gamesList = games.ToList();
+
+        //        _context.UpdateGames(games);
+        //        hasbeenUpdated = true;
+        //    }
+        //    return View(_context.Games);
+        //}
+
         // GET: Games
         public async Task<IActionResult> Index()
         {
@@ -38,7 +59,26 @@ namespace KlantenService_Steam_Framework.Controllers
             return View(games);
         }
 
-        // GET: Games/Details/5
+
+
+        //// GET: Games/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var game = await _context.Games
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (game == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(game);
+        //}
+
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -46,120 +86,17 @@ namespace KlantenService_Steam_Framework.Controllers
                 return NotFound();
             }
 
-            var game = await _context.Games
-                .FirstOrDefaultAsync(m => m.Id == id);
+            //var game = await _context.Games
+            //    .FirstOrDefaultAsync(m => m.Id == id);
+
+            var games = await _gameService.GetGamesAsync();
+            Game game = games.FirstOrDefault(g => g.Id == id);
             if (game == null)
             {
                 return NotFound();
             }
 
             return View(game);
-        }
-
-        // GET: Games/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Games/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Background_image,Games_count")] Game game)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(game);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(game);
-        }
-
-        // GET: Games/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var game = await _context.Games.FindAsync(id);
-            if (game == null)
-            {
-                return NotFound();
-            }
-            return View(game);
-        }
-
-        // POST: Games/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Background_image,Games_count")] Game game)
-        {
-            if (id != game.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(game);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!GameExists(game.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(game);
-        }
-
-        // GET: Games/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var game = await _context.Games
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (game == null)
-            {
-                return NotFound();
-            }
-
-            return View(game);
-        }
-
-        // POST: Games/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var game = await _context.Games.FindAsync(id);
-            if (game != null)
-            {
-                _context.Games.Remove(game);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         // GET: Games/GetGames
