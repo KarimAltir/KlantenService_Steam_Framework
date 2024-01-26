3e0f54c629ede86dcefd7f87f49764e521c493b9
Karim Altir <mhd.altir@student.ehb.be>
2024-01-26 22:05:13 +0100

    created my own Middelware to make sure that the data from API will save into the database

---
+++ Services/GameListMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using KlantenService_Steam_Framework.Data;
+using KlantenService_Steam_Framework.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupSpace23.Services
+{
+    public class GameListMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly GameService _gameService;
+        private readonly ApplicationDbContext _context;
+
+        public GameListMiddleware(RequestDelegate next, GameService gameService, ApplicationDbContext context)
+        {
+            _next = next;
+            _gameService = gameService;
+            _context = context;
+        }
+        
+        public async Task Invoke(HttpContext context)
+        {
+            // Haal de lijst van spellen op vanuit de externe API
+            var games = await _gameService.GetGamesAsync();
+
+            // Sla de lijst van spellen op in de database
+            await _context.UpdateGamesAsync(games);
+
+            // Ga door naar het volgende middelware-component in de pipeline
+            await _next(context);
+        }
+    }
+}
