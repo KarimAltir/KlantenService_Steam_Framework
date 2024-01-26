7b1c94ebf5a4251cc2b35fd8e9842e48c16b5c5e
Karim Altir <mhd.altir@student.ehb.be>
2024-01-26 22:11:57 +0100

    created middleware for the userState

---
+++ Services/MyUser.cs
@@ -0,0 +1,32 @@
+﻿using KlantenService_Steam_Framework.Areas.Identity.Data;
+using KlantenService_Steam_Framework.Data;
+using Microsoft.IdentityModel.Tokens;
+
+namespace KlantenService_Steam_Framework.Services
+{
+    public interface IMyUser
+    {
+        public KlantenServiceUser User();
+    }
+    public class MyUser : IMyUser
+    {
+        readonly ApplicationDbContext _context;
+        readonly IHttpContextAccessor _httpContext;
+        KlantenServiceUser user = null;
+
+        public MyUser(ApplicationDbContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        public KlantenServiceUser User()
+        {
+            string name = _httpContext.HttpContext.User.Identity.Name;
+            if (user == null || user.UserName != name)
+                user = _context.Users.First(u => u.UserName == (string.IsNullOrEmpty(name) ? "Dummy" : name));
+
+            return user;
+        }
+    }
+}
---
+++ Services/UserStats.cs
@@ -0,0 +1,58 @@
+﻿using KlantenService_Steam_Framework.Areas.Identity.Data;
+using KlantenService_Steam_Framework.Data;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+
+namespace KlantenService_Steam_Framework.Services
+{
+    public class UserStats
+    {
+        readonly RequestDelegate _next; // Verwijsing naar de volgdende middleware in de afhalndeling van de request
+        readonly IMyUser _myUser;
+        struct Statistics 
+        {
+            public KlantenServiceUser User { get; set; }
+            public DateTime Connected { get; set; }
+            public int NumberOfRequests { get; set; }
+            public DateTime LastConnected { get; set; }
+        }
+
+        static Dictionary<string, Statistics> DictStatistics;
+
+        //public static int GetNumberOfRequests { get { return DicStatistics } }
+
+        private Statistics stats;
+
+        public UserStats(RequestDelegate next) 
+        {
+            DictStatistics = new Dictionary<string, Statistics>();
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, IMyUser myUser)
+        {
+            try
+            {
+                Statistics stat = DictStatistics[myUser.User().UserName];
+                stat.LastConnected = DateTime.Now;
+                stat.NumberOfRequests++;
+            }
+            catch
+            {
+                Statistics stat = new Statistics();
+                stat.User = myUser.User();
+                stat.Connected = DateTime.Now;
+                stat.NumberOfRequests = 1;
+                stat.LastConnected = DateTime.Now;
+                DictStatistics[myUser.User().UserName] = stats;
+            }
+            
+
+            await _next(httpContext);
+        }
+
+        public static int GetCountofRequest(string name)
+        {
+            return DictStatistics[name].NumberOfRequests;
+        }
+    }
+}
