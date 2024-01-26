7c266a7620ed39b8e4f2cc7daf0da21bf745d676
Karim Altir <mhd.altir@student.ehb.be>
2024-01-26 22:09:55 +0100

    change few things from the classes

--- Models/Complaint.cs
+++ Models/Complaint.cs
@@ -1,27 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using KlantenService_Steam_Framework.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KlantenService_Steam_Framework.Models
 {
     public class Complaint
     {
         public int Id { get; set; }
+        [Display(Name = "Description")]
         public string Description { get; set; }
 
         // Foreign key naar Game
         [ForeignKey("Game")]
         public int GameId { get; set; }
+        [Display(Name = "Game")]
+        public string Name { get; set; } 
         // Navigatie-eigenschap naar Game
         public Game? Game { get; set; }
 
         // Foreign key naar ProblemType
         [ForeignKey("ProblemType")]
         public int ProblemTypeId { get; set; }
+        [Display(Name = "ProblemType")]
+        public string TypeName { get; set; }
         public ProblemType? ProblemType { get; set; }
-        
+
+        [Display(Name = "Email")]
         public string Email { get; set; }
         public string Status { get; set; }
 
-        ////adding user name
-        //public string UserName { get; set; }
+        [ForeignKey("KlantenServiceUser")]
+        public string UserId { get; set; }
+        //adding user name
+        public KlantenServiceUser? User { get; set; }
     }
 }
--- Models/Game.cs
+++ Models/Game.cs
@@ -16,8 +16,16 @@ namespace KlantenService_Steam_Framework.Models
         [Display(Name = "Afbeelding")]
         public string Background_image { get; set; }
 
-        [Display(Name = "Spel optellen")]
-        public int Games_count { get; set; }
+        [Display(Name = "Releasedatum")]
+        public string Released { get; set; }
+
+        //rating
+        [Display(Name = "Rating")]
+        public double Rating { get; set; }
+
+        //slug
+        [Display(Name = "Slug")]
+        public string Slug { get; set; }
 
         // De verzameling Complaints die aan dit Game zijn gekoppeld
         //public ICollection<Complaint> Complaints { get; set; }
