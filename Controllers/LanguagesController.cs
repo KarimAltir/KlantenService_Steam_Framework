d49a766cba6ec4947ccb2c8091766cdc44c01249
Karim Altir <mhd.altir@student.ehb.be>
2024-01-26 22:09:25 +0100

    creating controller for Languages

---
+++ Controllers/LanguagesController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KlantenService_Steam_Framework.Data;
+using KlantenService_Steam_Framework.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Localization;
+
+namespace KlantenService_Steam_Framework.Controllers
+{
+    public class LanguagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LanguagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Languages
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Languages.ToListAsync());
+        }
+
+        // GET: Languages/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = await _context.Languages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            return View(language);
+        }
+
+        // GET: Languages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Languages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,IsSystemLanguage,IsAvailable")] Language language)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(language);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(language);
+        }
+
+        // GET: Languages/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = await _context.Languages.FindAsync(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+            return View(language);
+        }
+
+        // POST: Languages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,IsSystemLanguage,IsAvailable")] Language language)
+        {
+            if (id != language.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(language);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LanguageExists(language.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(language);
+        }
+
+        // GET: Languages/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = await _context.Languages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            return View(language);
+        }
+
+        // POST: Languages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var language = await _context.Languages.FindAsync(id);
+            if (language != null)
+            {
+                _context.Languages.Remove(language);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LanguageExists(string id)
+        {
+            return _context.Languages.Any(e => e.Id == id);
+        }
+
+        [AllowAnonymous]
+        public IActionResult ChangeLanguage(string id, string returnUrl)
+        {
+            string culture = Thread.CurrentThread.CurrentCulture.ToString();
+            try
+            {
+                culture = id + culture.Substring(2, 3);
+            }
+            catch
+            {
+                culture = id + "-BE";
+            }
+
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
